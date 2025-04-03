39ffceb4d3979ab307fbe05a70c2903d3d88c0a9
Kachambalele <ekachambalele@gmail.com>
2025-04-03 12:55:18 +0100

    Update BasedeDaddos.py

--- Base de Dados/BasedeDaddos.py
+++ Base de Dados/BasedeDaddos.py
@@ -1,3 +1,19 @@
 import streamlit as st
 import panda as pd
 import numpy as np
+
+st.title("Base de Dados")
+
+st.sidebar.header("Leagues")
+selected_league = st.sidebar.selectbox("League", ['England', 'Germany', 'Italy', 'Belgiun', 'Netherland', 'Spain', 'France', 'Portugal', 'Turkie', 'Scotiland'])
+st.sidebar.header("Season")
+selected_league = st.sidebar.selectbox("League", ['England', 'Germany', 'Italy', 'Belgiun', 'Netherland', 'Spain', 'France', 'Portugal', 'Turkie', 'Scotiland'])
+st.sidebar.header("Season")
+selected_league = st.sidebar.selectbox("League", ['2021/2022', '2022/2023', '2023/2024', '2024/205'])
+def load_data(league, season):
+  url = "https://www.football-data.co.uk/mmz4281/"season"/"league".csv"
+  data = pd.read_csv(url)
+  return data
+
+df = load_data(selected_league, selected_season)
+  
